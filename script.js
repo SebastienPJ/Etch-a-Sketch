35a7ac31b1130a3164f9dbdf5a6ff0a1cc6f02f9
Sebastien Pierre-Jacques <sebastienpj@protonmail.com>
2021-05-31 12:13:56 -0500

    Added changeColor function

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container");
 
 
-let dimensionDesired = 4
+let dimensionDesired = 16
 createGrid(Math.pow(dimensionDesired, 2));
 
 
 
 
-
-// Creates # of boxes desired inside of container
+/* Creates # of boxes desired inside of container 
+    and adds mouseover event listener to them */
 function createGrid(numberOfBoxes) {
 
   for (let i = 0; i < numberOfBoxes; i++) {
@@ -17,9 +17,46 @@ function createGrid(numberOfBoxes) {
 
     let size = `calc(${num2}% - 2px)`;
     let div = document.createElement("div");
-    div.setAttribute("style", `border: 1px solid black; background-color: blue; height: ${size}; flex-basis: ${size}; flex-grow: 1;`);
-
+    div.setAttribute("style", `border: 1px solid black; background-color: rgb(255, 255, 255); height: ${size}; flex-basis: ${size}; flex-grow: 1;`);
+    div.classList.add("square");
     container.appendChild(div); 
+    div.addEventListener("mouseover", changeColor)
   }
   
+}
+
+
+
+
+/****** Checks color of current square, if default color changes to light gray 
+        and becomes darker at each pass ******/
+function changeColor(e) {
+  let currentColor = e.target.style.backgroundColor;
+  let rgbArray = []; 
+
+  let newRGB = [];
+  let newRgbString;
+ 
+  if (currentColor == "rgb(255, 255, 255)") {
+    e.target.style.backgroundColor = "rgb(100, 100, 100)";
+
+  } else {
+
+    // Takes square's current rgb and puts values into an array
+    rgbArray = currentColor.substring(4, currentColor.length-1)
+          .replace(/ /g, '')
+          .split(',');
+
+    // Makes current color darker
+    for (let i = 0; i < 3; i++ ){
+      let newColor = rgbArray[i] - 10
+      newRGB.push(newColor)
+    };
+
+    newRgbString = newRGB.toString();
+
+
+    e.target.style.backgroundColor = `rgb(${newRgbString})`
+
+  }
 }
